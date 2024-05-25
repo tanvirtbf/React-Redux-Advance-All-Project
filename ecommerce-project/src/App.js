8c9f63507dfdd6d5fe5de878bff5eb96909ad9f7
tanvirtbf <tanver018765@gmail.com>
2024-05-25 09:30:11 +0600

    routing perform with old way

--- ecommerce-project/src/App.js
+++ ecommerce-project/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element: <h1>Home Page</h1>
+  },
+  {
+    path: '/images',
+    element: <h1>Images</h1>
+  },
+])
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <RouterProvider router={router}/>
+    </>
+  )
 }
 
-export default App;
+export default App
+
